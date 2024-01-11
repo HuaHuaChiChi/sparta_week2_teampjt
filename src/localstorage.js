b6d968df637f685d9920d0d4708698f80a28a992
HuaHuaChiChi <seunghuigim995@gmail.com>
2024-01-12 00:56:10 +0900

    삭제기능 추가

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,9 +1,8 @@
 
 const commentForm = document.getElementById('reviewInput');
 const usernameElement = document.getElementById('username');
-
-console.log(commentForm);
-console.log(usernameElement);
+const commentList = document.getElementById('commentList');
+const userPassword = document.getElementById('password');
 
 function submitReview() {
     const reviewContent = commentForm.value.trim();
@@ -11,6 +10,7 @@ function submitReview() {
 
     if (reviewContent !== "" && username !== "") {
         const reviewData = {
+            id : new Date().getTime(),
             name: username,
             content: reviewContent,
         };
@@ -32,25 +32,43 @@ function saveReview(reviewData) {
 }
 
 function loadReviews() {
-    const commentList = document.getElementById('commentList');
     commentList.innerHTML = ''; // 기존 댓글 목록 비우기
 
-    const existingReviews = JSON.parse(localStorage.getItem('reviews')) || [];
+    const existingReviews = JSON.parse(localStorage.getItem("reviews")) || [];
 
     if (existingReviews.length > 0) {
         existingReviews.forEach(review => {
             const reviewItem = document.createElement('div');
             reviewItem.className = 'reviewItem';
             reviewItem.innerHTML = `
-            <p>${review.name}: ${review.content}</p> 
-            <button id = "reviewDelete">삭제 </button>
-            <button id = "reviewUpdate">수정 </button>`;
+            <p>${review.name}: ${review.content}, ${review.id}</p> 
+            <button class = "deleteButton" data-review-id="${review.id}">삭제 </button>
+            <button class = "reviewUpdate">수정 </button>`;
             commentList.appendChild(reviewItem);
         });
+        //추가 
+        const deleteButtons = document.querySelectorAll('.deleteButton');
+        deleteButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                const reviewId = parseInt(this.dataset.reviewId);
+                deleteReview(reviewId);
+                loadReviews(); // 리뷰 목록 갱신
+            });
+        });
+        //추가
     } else {
         commentList.innerHTML = '<p>아직 리뷰가 없습니다.</p>';
     }
-}
+};
+
+//추가
+function deleteReview(reviewId) {
+    const existingReviews = JSON.parse(localStorage.getItem('reviews')) || [];
+    const updatedReviews = existingReviews.filter(review => review.id !== reviewId);
+    localStorage.setItem('reviews', JSON.stringify(updatedReviews));
+};
+//추가
+
 
 
 const form = document.querySelector('form');
@@ -60,26 +78,3 @@ form.addEventListener('submit', function(event) {
 });
 
 loadReviews(); // 페이지 로드 시 리뷰 목록 불러오기
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// localStorage.setItem("test", "테스트입니다");
-
-// const test = localStorage.getItem("test");
-// console.log(test);
-
-
