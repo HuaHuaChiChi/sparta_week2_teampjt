bf9647a80d2f6202177d5c92726eb28301f2a7be
tjdwns335 <http://github.com/tjdwns335>
2024-01-15 14:34:56 +0900

    Wow

--- src/detail.js
+++ src/detail.js
@@ -85,3 +85,27 @@ const createMovieDetail = (detail, credits) => {
   </div>`;
   return detail_html;
 };
+
+const endpoints = {
+  nowPlaying: {
+    authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjRkY2E3YzRhYjRjOGY3Zjc5NjA0ZWRkNTQwMjE2NiIsInN1YiI6IjY1OTNiNzljZWJiOTlkNWUxN2EwMTRlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzYyp6rUTuS2MYX8KCIEgGrkns1anoyP2yhoqvkXv-Q",
+    url: "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1",
+  },
+  popular: {
+    authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY",
+    url: "https://api.themoviedb.org/3/movie/popular?language=ko-KR&include_adult=false",
+  },
+  topRated: {
+    authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY",
+    url: "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1&include_adult=false",
+  },
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (const key in endpoints) {
+    const button = document.querySelector(`#${key} > a`);
+    button.addEventListener('click', () => generateMovieCards(endpoints[key].authorization, endpoints[key].url));
+    button.href = "../index.html";
+  }
+})
--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
 import { generateMovieCards } from './movie.js';
 import { handleSearch } from "./searchFunction.js";
 
-const nowPlayingAuthorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjRkY2E3YzRhYjRjOGY3Zjc5NjA0ZWRkNTQwMjE2NiIsInN1YiI6IjY1OTNiNzljZWJiOTlkNWUxN2EwMTRlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzYyp6rUTuS2MYX8KCIEgGrkns1anoyP2yhoqvkXv-Q";
-const nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1";
-const popularAuthorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY";
-const popularUrl = "https://api.themoviedb.org/3/movie/popular?language=ko-KR&include_adult=false";
-const topRateAuthorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY";
-const topRateUrl = "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1&include_adult=false";
+const endpoints = {
+  nowPlaying: {
+    authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjRkY2E3YzRhYjRjOGY3Zjc5NjA0ZWRkNTQwMjE2NiIsInN1YiI6IjY1OTNiNzljZWJiOTlkNWUxN2EwMTRlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzYyp6rUTuS2MYX8KCIEgGrkns1anoyP2yhoqvkXv-Q",
+    url: "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1",
+  },
+  popular: {
+    authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY",
+    url: "https://api.themoviedb.org/3/movie/popular?language=ko-KR&include_adult=false",
+  },
+  topRated: {
+    authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY",
+    url: "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1&include_adult=false",
+  },
+};
 
-generateMovieCards(nowPlayingAuthorization, nowPlayingUrl);
 
-let topRateBtn = document.querySelector('#toprate > a');
-let popularBtn = document.querySelector('#popular> a');
-let nowPlayBtn = document.querySelector('#nowPlaying > a');
+generateMovieCards(endpoints.nowPlaying.authorization, endpoints.nowPlaying.url);
 
-topRateBtn.addEventListener('click', () => generateMovieCards(topRateAuthorization, topRateUrl));
-popularBtn.addEventListener('click', () => generateMovieCards(popularAuthorization, popularUrl));
-nowPlayBtn.addEventListener('click', () => generateMovieCards(nowPlayingAuthorization, nowPlayingUrl));
+for (const key in endpoints) {
+  const button = document.querySelector(`#${key} > a`);
+  button.addEventListener('click', () => generateMovieCards(endpoints[key].authorization, endpoints[key].url));
+}
+
 
 const searchInput = document.querySelector("#search-input");
 searchInput.focus();
