479ae9f8c641f5e363b67da9e74079dfb572687c
tjdwns335 <http://github.com/tjdwns335>
2024-01-15 12:07:13 +0900

    합쳐 놓음

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { generateMovieCards } from './movie.js';
+
+const nowPlayingAuthorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjRkY2E3YzRhYjRjOGY3Zjc5NjA0ZWRkNTQwMjE2NiIsInN1YiI6IjY1OTNiNzljZWJiOTlkNWUxN2EwMTRlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BzYyp6rUTuS2MYX8KCIEgGrkns1anoyP2yhoqvkXv-Q";
+const nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1";
+const popularAuthorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY";
+const popularUrl = "https://api.themoviedb.org/3/movie/popular?language=ko-KR&include_adult=false";
+const topRateAuthorization = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY";
+const topRateUrl = "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1&include_adult=false";
+
+generateMovieCards(nowPlayingAuthorization, nowPlayingUrl);
+
+let topRateBtn = document.querySelector('#toprate > a');
+let popularBtn = document.querySelector('#popular> a');
+let nowPlayBtn = document.querySelector('#nowPlaying > a');
+
+topRateBtn.addEventListener('click', () => generateMovieCards(topRateAuthorization, topRateUrl));
+popularBtn.addEventListener('click', () => generateMovieCards(popularAuthorization, popularUrl));
+nowPlayBtn.addEventListener('click', () => generateMovieCards(nowPlayingAuthorization, nowPlayingUrl));
--- src/movie.js
+++ src/movie.js
@@ -1,9 +1,8 @@
-// 1. 영화 데이터를 가져와서 화면에 나타내기
 const sortButtons = document.querySelector(".header-sort");
 const cardList = document.querySelector("#card-list");
 
-export const generateMovieCards = async () => {
-  let movies = await fetchMovieData();
+export const generateMovieCards = async (Authorization, url) => {
+  let movies = await fetchMovieData(Authorization, url);
 
   if (cardList) {
     function renderMovieCards() {
@@ -62,30 +61,29 @@ export const generateMovieCards = async () => {
   }
 };
 
-async function fetchMovieData() {
+async function fetchMovieData(Authorization, url) {
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmM2ZTFkNjQzMTNkMDY1ZjczYjkyYjliNTM4YmJjNSIsInN1YiI6IjY1OTNkMDkyZmMzMWQzNzI4NTQ2YjQ3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CkZC7SdOdnrzr2YHFLyd94sIAFIYTAK2sOqJHujnVCY"
+      Authorization: Authorization,
     }
   };
-  const response = await fetch("https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1&include_adult=false", options);
+  const response = await fetch(url, options);
   const data = await response.json();
   return data.results;
 }
+// 가져온 Popular API를 실행
 
-generateMovieCards();
 
-// scroll 내려가면 정렬버튼 header에 붙음
+
 document.addEventListener("DOMContentLoaded", () => {
   const sortButton = document.querySelector("#sortButton");
   if (sortButton) {
     window.addEventListener("scroll", () => {
       const scrollY = window.scrollY || window.pageYOffset;
+
       scrollY >= 418 ? sortButton.classList.add("fixed") : sortButton.classList.remove("fixed");
     });
   }
 });
-
